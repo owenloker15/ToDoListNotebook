ac359c8fc3c7912e89104970bfc7ff65a2826a00
magolden <magolden@ncsu.edu>
2021-04-12 17:57:12 -0400

    Implement and test NotebookWriter

--- Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookWriter.java
+++ Project2/src/edu/ncsu/csc216/wolf_tasks/model/io/NotebookWriter.java
@@ -4,6 +4,9 @@
 package edu.ncsu.csc216.wolf_tasks.model.io;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 
 import edu.ncsu.csc216.wolf_tasks.model.tasks.TaskList;
 import edu.ncsu.csc216.wolf_tasks.model.util.ISortedList;
@@ -30,6 +33,25 @@ public class NotebookWriter {
 	 * @param taskList SortedList of TasksLists in the Notebook
 	 */
 	public static void writeNotebookFile(File fileName, String notebookName, ISortedList<TaskList> taskList) { //may need to be AbstractTaskList
+		FileOutputStream fos = null;
+		PrintWriter out = null;
 		
+		try {
+			fos = new FileOutputStream(fileName);
+            out = new PrintWriter(fos);
+            
+            out.write("! " + notebookName);
+            for (int i = 0; i < taskList.size(); i++) {
+            	out.write("# " + taskList.get(i).getTaskListName() + taskList.get(i).getCompletedCount());
+            	
+            	for (int j = 0; j < taskList.get(i).getTasks().size(); j++) {
+            		out.write("* " + taskList.get(i).getTasks().get(j).toString());
+            	}
+            }
+            
+            out.close();
+		} catch (FileNotFoundException e) {
+			throw new IllegalArgumentException("Unable to save file.");
+		}
 	}
 }
